e9886c73ccfc2e01c2ba53a49a8f20090188fda1
Tay <tay.james.d@gmail.com>
2019-09-16 10:35:06 -0600

    Adds iteration over my five_day_forecast -- working in pry

--- app/facade/gifs_facade.rb
+++ app/facade/gifs_facade.rb
@@ -29,6 +29,12 @@ class GifsFacade
     five_day_forecast = forecast_data[:daily][:data].map do |day|
       DailyForecast.new(day)
     end.take(5)
+
+    five_day_summary = five_day_forecast.map do |day|
+      day.summary
+    end
+
+
     binding.pry
 
     giphy_conn = Faraday.new(url: "https://api.giphy.com/v1/gifs/search") do |f|
@@ -37,7 +43,7 @@ class GifsFacade
       f.adapter Faraday.default_adapter
     end
 
-    giphy_response = giphy_conn.get("?location=#{@location}")
+    # giphy_response = giphy_conn.get("?location=#{@location}")
 
     JSON.parse(giphy_response.body, symbolize_names: true)
   end
