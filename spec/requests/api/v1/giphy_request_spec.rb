c51acf97c4305cb7e4106839c0ed38e4ed18d8ce
Tay <tay.james.d@gmail.com>
2019-09-16 09:33:04 -0600

    Adds skeleton of Giphy request spec -- WIP

---
+++ spec/requests/api/v1/giphy_request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Returns a gif" do
+  it "can return a gif given a location" do
+    get '/api/v1/gifs?location=denver,co'
+
+    expect(response).to be_successful
+    gif_data = JSON.parse(response.body, symbolize_names: true)
+
+    
+  end
+end
