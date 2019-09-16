c103852f73b46f89dd96e09108c8c28e6e01f3f7
Tay <tay.james.d@gmail.com>
2019-09-15 22:50:37 -0600

    Adds ForecastController with index action

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    forecast = Forecast.new(params[:location])
+    forecast = ForecastFacade.new(params[:location])
+    render json: ForecastSerializer.new(forecast)
   end
 end
