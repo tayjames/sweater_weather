2d1e1c9ba33c5859e27b066ebe7028b26d51f2a8
Tay <tay.james.d@gmail.com>
2019-09-16 10:12:56 -0600

    Adds daily_time attribute to DailyForecast PORO

--- app/models/daily_forecast.rb
+++ app/models/daily_forecast.rb
@@ -1,10 +1,11 @@
 class DailyForecast
-  attr_reader :high, :low, :summary, :precipitation
-  
+  attr_reader :high, :low, :summary, :precipitation, :daily_time
+
   def initialize(attributes)
     @high = attributes[:temperatureHigh]
     @low = attributes[:temperatureLow]
     @summary = attributes[:summary]
     @precipitation = attributes[:precipProbability]
+    @daily_time = attributes[:time]
   end
 end
