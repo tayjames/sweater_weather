1b2cb394a5dafe1bbde97d0aebab3c086fe8ba6b
Tay <tay.james.d@gmail.com>
2019-09-15 22:47:37 -0600

    Adds testing for forecast request

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "Geocoding API" do
-  it "can send a city and state" do
+RSpec.describe "Returns a forecast" do
+  it "can return a forecast given a city and state" do
     get '/api/v1/forecast?location=denver,co'
 
-    expect(response).to be_successful 
+    expect(response).to be_successful
+    forecast_data = JSON.parse(response.body, symbolize_names: true)
+    # binding.pry
+
+    expect(response.content_type).to eq("application/json")
+    expect(forecast_data.class).to eq(Hash)
+    expect(forecast_data[:data][:attributes].class).to eq(Hash)
+    expect(forecast_data[:data][:attributes].keys).to eq([:current_forecast, :daily_forecast, :hourly_forecast])
+    expect(forecast_data[:data][:attributes][:current_forecast].keys).to eq([:icon, :temperature, :feels_like, :humidity, :visibility, :uv_index, :summary])
+    expect(forecast_data[:data][:attributes][:daily_forecast].count).to eq(5)
+    expect(forecast_data[:data][:attributes][:daily_forecast][0].keys).to eq([:high, :low, :summary, :precipitation])
+    expect(forecast_data[:data][:attributes][:hourly_forecast].count).to eq(8)
+    expect(forecast_data[:data][:attributes][:hourly_forecast][0].keys).to eq([:time, :hourly_temperature])
   end
 end
