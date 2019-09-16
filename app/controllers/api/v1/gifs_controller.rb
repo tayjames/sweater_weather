f06a54fc39a832cc6005dfcc608c2606fa2aee82
Tay <tay.james.d@gmail.com>
2019-09-16 11:43:34 -0600

    Updates index method to now include a GifSerializer

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::GifsController < ApplicationController
   def index
-    render json: GifsFacade.new(params[:location]).gif
+    # render json: GifsFacade.new(params[:location]).gif
+    gif = GifsFacade.new(params[:location])
+    render json: GifsSerializer.new(gif)
   end
 end
