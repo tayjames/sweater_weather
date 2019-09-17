f10d16433b996511de831c40190fb6a8f06465d0
Tay <tay.james.d@gmail.com>
2019-09-17 11:04:46 -0600

    Removes commented out text and adds extra arg to AntipodeForecast

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::AntipodeController < ApplicationController
   def index
-    # render json: AntipodeFacade.new(params[:location]).antipode
     antipode = AntipodeFacade.new(params[:location])
     render json: AntipodeSerializer.new(antipode)
   end
