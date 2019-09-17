8a1d5a9f351f3e2b83e3b73c0ec02d2bdce1d1b1
Tay <tay.james.d@gmail.com>
2019-09-17 10:18:58 -0600

    Adds Serializer to AntipodeController

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::AntipodeController < ApplicationController
   def index
-    render json: AntipodeFacade.new(params[:location]).antipode
+    # render json: AntipodeFacade.new(params[:location]).antipode
+    antipode = AntipodeFacade.new(params[:location])
+    render json: AntipodeSerializer.new(antipode)
   end
 end
