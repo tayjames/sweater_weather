83cca00a46829184f788a0e4aa5cca833b2db330
Tay <tay.james.d@gmail.com>
2019-09-17 10:20:03 -0600

    Adds all api calls necessary to find the current weather and temperature of the antipode of HK

--- app/facade/antipode_facade.rb
+++ app/facade/antipode_facade.rb
@@ -18,13 +18,32 @@ class AntipodeFacade
     coordinate_data = JSON.parse(google_response.body, symbolize_names: true)[:results]
     latitude = coordinate_data[0][:geometry][:location][:lat]
     longitude = coordinate_data[0][:geometry][:location][:lng]
-    binding.pry
 
-    # darksky_conn = Faraday.new(url: "https://api.darksky.net") do |f|
-    #   f.adapter Faraday.default_adapter
-    # end
-    #
-    # darksky_response = darksky_conn.get("/forecast/#{ENV['DS_KEY']}/#{latitude},#{longitude}")
-    # forecast_data = JSON.parse(darksky_response.body, symbolize_names: true)
+    amypode_conn = Faraday.new(url: "http://amypode.herokuapp.com") do |f|
+      f.headers["api_key"] = ENV['AMY_API_KEY']
+      f.adapter Faraday.default_adapter
+    end
+
+    amypode_response = amypode_conn.get("/api/v1/antipodes?lat=#{latitude}&long=#{longitude}")
+    antipode_data = JSON.parse(amypode_response.body, symbolize_names: true)[:data][:attributes]
+    antipode_lat = antipode_data[:lat]
+    antipode_long = antipode_data[:long]
+
+    reverse_conn = Faraday.new(url: "https://maps.googleapis.com/maps/api/geocode/json") do |f|
+      f.adapter Faraday.default_adapter
+    end
+
+    reverse_response = reverse_conn.get("?latlng=#{antipode_lat},#{antipode_long}&key=#{ENV['API_KEY']}")
+    reverse_data = JSON.parse(reverse_response.body, symbolize_names: true)[:results][0][:geometry][:location]
+    reverse_lat = reverse_data[:lat]
+    reverse_long = reverse_data[:lng]
+
+    darksky_conn = Faraday.new(url: "https://api.darksky.net") do |f|
+      f.adapter Faraday.default_adapter
+    end
+
+    darksky_response = darksky_conn.get("/forecast/#{ENV['DS_KEY']}/#{reverse_lat},#{reverse_long}")
+    forecast_data = JSON.parse(darksky_response.body, symbolize_names: true)
+    binding.pry
   end
 end
