823798cb9ec704eb0fec8e311ec17b411bf560b5
Tay <tay.james.d@gmail.com>
2019-09-18 10:28:56 -0600

    Comments out broken tests

---
+++ spec/models/currentweather_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# RSpec.describe CurrentWeather, type: :model do
+  # it "exists" do
+    # attrs = {
+    #
+    # }
+    #
+    # current = CurrentWeather.new(attrs)
+    #
+    # expect(current).to be_a CurrentWeather
+    # expect(current.icon).to eq("partly-cloudy-day")
+    # expect(current.feels_like).to eq
+  # end
+# end
