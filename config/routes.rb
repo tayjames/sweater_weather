3a3c269f3c7122d3bd5c9c4d854712b5e3d9da19
Tay <tay.james.d@gmail.com>
2019-09-18 10:26:27 -0600

    Adds routes for backgrounds, users, sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :forecast, only: [:index]
       resources :antipode, only: [:index]
+      resources :backgrounds, only: [:index]
+      resources :users, only: [:create]
+      resources :sessions, only: [:create]
     end
   end
 end
